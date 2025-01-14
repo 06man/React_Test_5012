e817f0a3049a7063ebcb8fbc8d4386dc5f0ca0eb
이상용 <lsy3709@naver.com>
2025-01-14 16:11:10 +0900

    7교시 , jsx 기본 문법2-3, || 이용한 조건부 렌더링

--- hello-vite-test/src/App.jsx
+++ hello-vite-test/src/App.jsx
@@ -13,42 +13,54 @@ function App() {
   // 실제 동작은. 
   // return React.createElement("div", null, "Hello ", React.createElement ("b", null, "react"));
 
-  const name = '리액트';
+  // const name = "리액트";
+  // const name = 0;
+  const name = undefined;
 
-  return (
-    // 순서1
-    // <div>
-    //   Hello 리액트 
-    // </div>
+  // return (
+  // 순서1
+  // <div>
+  //   Hello 리액트 
+  // </div>
 
-    //순서2
-    // <div> 
-    // 순서3
-    // <fragment>
-    // 순서4
-    // <>
-    //   <h1>리액트 안녕!</h1>
-    //   <h2>잘 작동하니? {name}</h2>
-    // </>
-    // </fragment>
-    // </div>
-    //순서5
-    // <div>
-    //   {name === '리액트2' ? (
-    //     <h1>리액트입니다.</h1>
-    //   ) : (
-    //     <h2>리액트가 아닙니다.</h2>
-    //   )}
-    // </div>
+  //순서2
+  // <div> 
+  // 순서3
+  // <fragment>
+  // 순서4
+  // <>
+  //   <h1>리액트 안녕!</h1>
+  //   <h2>잘 작동하니? {name}</h2>
+  // </>
+  // </fragment>
+  // </div>
+  //순서5
+  // <div>
+  //   {name === '리액트2' ? (
+  //     <h1>리액트입니다.</h1>
+  //   ) : (
+  //     <h2>리액트가 아닙니다.</h2>
+  //   )}
+  // </div>
 
-    //순서6
-    // A && B, A 참인 경우만, B를 그려준다.
-    <div>
-      {
-        name === '리액트' && <h1>리액트입니다.</h1>
-      }
-    </div>
-  )
+  //순서6
+  // A && B, A 참인 경우만, B를 그려준다.
+  // 조건이 거짓 일 때, null 반환를 하면, 화면에 아무것도 그리지 않음. 
+
+  // <div>
+  //   {
+  //     //순서7
+  //     // name === '리액트2' && <h1>리액트입니다.</h1>
+  //     // name && <h1>리액트입니다.</h1>
+  //     // name;
+  //   }
+  // </div>
+  // 순서8, undefined  출력 시 오류 발생 확인.-> 널 처럼 아무것도 출력을 안하는 상태. 
+
+
+  // )
+  // 순서 9, or 연산자를 이용하는 방법. 앞에 변수가 출력이 안되는 상황을 대비할 경우. 
+  return name || '값이undefined입니다.';
 }
 
 export default App
