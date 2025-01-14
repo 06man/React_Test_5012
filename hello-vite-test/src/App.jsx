468b8f73beba155d6e4afcd087bf6814879e41bb
이상용 <lsy3709@naver.com>
2025-01-14 15:20:43 +0900

    6교시, jsx 기본 문법1

--- hello-vite-test/src/App.jsx
+++ hello-vite-test/src/App.jsx
@@ -2,33 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import React from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
+  //JSX 라는 문법은 , 우리가 보기에는 마치 HTML 문법 처럼 보이지만, 
+
+
+  // JSX 문법이고, 실제 동작은 위의 
+  // 실제 동작은. 
+  // return React.createElement("div", null, "Hello ", React.createElement ("b", null, "react"));
   return (
+    // <div>
+    //   Hello 리액트 
+    // </div>
+    // <div>
+    // <fragment>
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
     </>
+    // </fragment>
+    // </div>
   )
 }
 
