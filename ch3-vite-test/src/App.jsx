079effdc30d7dd86860ef1bb8ad30603a5bbe44c
이상용 <lsy3709@naver.com>
2025-01-15 10:30:20 +0900

    2교시, props , 부모 - 자식으로 데이터 전달 확인해보기

--- ch3-vite-test/src/App.jsx
+++ ch3-vite-test/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
     <>
       <h1>{name}</h1>
       <div className="react">{name}</div>
-      <Mycomponent />
+      {/* 자식 컴포넌트 */}
+      <Mycomponent name="첫 데이터 전달하기 속성은 name으로 전달함." />
     </>
   )
 }
--- ch3-vite-test/src/component/Mycomponent.jsx
+++ ch3-vite-test/src/component/Mycomponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Mycomponent = () => {
+const Mycomponent = (props) => {
     return (
         <div>
             샘플 나의 첫 컴포넌트 만들기.
+            데이터 전달 받기 : {props.name}
         </div>
     );
 };
