619b14706cfb736bbb3627fed37de0be534b27df
이상용 <lsy3709@naver.com>
2025-01-15 14:33:15 +0900

    5교시, 이벤트 핸들링, EventPractice 컴포넌트 onChange 확인

--- ch4-vite-test/src/App.jsx
+++ ch4-vite-test/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import EventPractice from './component/EventPractice'
 import Say from './component/Say'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <h1 className='react'>ch4 이벤트 핸들러 해보기</h1>
       <Say />
+      <EventPractice />
     </>
   )
 }
