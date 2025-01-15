8906f02faeefab01c9365be5e1a555d7c32c030c
이상용 <lsy3709@naver.com>
2025-01-15 14:59:38 +0900

    6교시, 이벤트 핸들링, EventPractice 컴포넌트 확인3 함수 분리

--- ch4-vite-test/src/component/EventPractice.jsx
+++ ch4-vite-test/src/component/EventPractice.jsx
@@ -7,12 +7,33 @@ class EventPractice extends Component {
         message: ''
     };
 
+    // 추가 2-1
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    // 추가 2-2
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+    // 추가 2-3
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
                 <h2>message : {this.state.message}</h2>
-                <input
+                {/* 방법1 */}
+                {/* <input
                     type="text"
                     name="message"
                     placeholder="아무거나 입력해 보세요"
@@ -26,6 +47,24 @@ class EventPractice extends Component {
                         });
                     }}
                 />
+                <button
+                    onClick={() => {
+                        alert(this.state.message);
+                        this.setState({ message: "" });
+                    }}
+                >
+                    초기화
+                </button> */}
+
+                {/* 방법2, 함수를 따로 분리해서 작업 */}
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해 보세요"
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                />
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
