b8b2663d1ffb858fb981390fa253f98579869a35
이상용 <lsy3709@naver.com>
2025-01-16 11:30:20 +0900

    3교시, ch5 ref 달기 스크롤 박스에서 스크롤 테스트, 클래스 형 컴포넌트로 작업

--- ch5-vite-test/src/App.jsx
+++ ch5-vite-test/src/App.jsx
@@ -1,16 +1,24 @@
+// 교체 작업 3
+import React, { Component } from 'react';
 import './App.css'
 import RefSample from './component/RefSample'
 import ValidationSample from './component/ValidationSample'
+import ScrollBox from './component/ScrollBox';
 
-function App() {
-
-  return (
+// function App() {
+// 함수형 컴포넌트 -> 클래스 형 컴포넌트 변경. 
+// 교체 작업 1
+class App extends Component {
+  // 교체 작업 2
+  return() {
     <>
       <h1 className='react'>ch5 ref, DOM 요소에 이름달기</h1>
-      <ValidationSample />
-      <RefSample />
+      {/* <ValidationSample />
+      <RefSample /> */}
+      {/* 교체 작업 4 */}
+      <ScrollBox />
     </>
-  )
+  }
 }
 
 export default App
