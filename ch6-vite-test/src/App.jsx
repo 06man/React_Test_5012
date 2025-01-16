62c823db5eb2fef55337faab8b43e2728ab060ff
이상용 <lsy3709@naver.com>
2025-01-16 15:11:23 +0900

    6교시, ch6 컴포넌트 반복, map, concat 내장 함수 이용해서, 새로운 배열 이용하기.

--- ch6-vite-test/src/App.jsx
+++ ch6-vite-test/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import IterationSample from '../component/IterationSample'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className='react'>ch6 컴포넌트 반복 </h1>
+      <IterationSample />
     </>
   )
 }
