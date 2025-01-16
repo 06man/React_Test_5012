8e6c058d4ff6c5235f4489db30a98b0ab2713a4d
이상용 <lsy3709@naver.com>
2025-01-16 10:49:20 +0900

    2교시, ch5  준비 작업2

--- ch5-vite-test/src/App.jsx
+++ ch5-vite-test/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ValidationSample from './component/ValidationSample'
 
 function App() {
 
   return (
     <>
       <h1 className='react'>ch5 ref, DOM 요소에 이름달기</h1>
+      <ValidationSample />
     </>
   )
 }
