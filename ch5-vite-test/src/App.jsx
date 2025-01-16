5197546bc6183de263081518af121fce5b74fac7
이상용 <lsy3709@naver.com>
2025-01-16 11:44:53 +0900

    3교시, ch5 ref 달기 자식 컴포넌트에 ref 달아서, 부모에서 자식 컴포넌트에 ref로 접근해서, 함수 호출, 맨위,맨아래이동

--- ch5-vite-test/src/App.jsx
+++ ch5-vite-test/src/App.jsx
@@ -9,6 +9,7 @@ import ScrollBox from './component/ScrollBox';
 // 함수형 컴포넌트 -> 클래스 형 컴포넌트 변경. 
 // 교체 작업 1
 class App extends Component {
+
   // 교체 작업 2
   render() {
     return (
@@ -17,7 +18,12 @@ class App extends Component {
         {/* <ValidationSample />
       <RefSample /> */}
         {/* 교체 작업 4 */}
-        <ScrollBox />
+        {/* 부모에서 자식 컴포넌트 방향으로 props 전달하기.  */}
+        {/* 컴포넌트에 접근하기 위해서, 컴포넌트에 ref 달기.  */}
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        {/* 부모에서 자식 컴포넌트의 ref에 접근해서, 자식 컴포넌트 안에 메서드 호출하기 */}
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button>
+        <button onClick={() => this.scrollBox.scrollToTop()}>맨위로</button>
       </>
     )
   }
--- ch5-vite-test/src/component/ScrollBox.jsx
+++ ch5-vite-test/src/component/ScrollBox.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class ScrollBox extends Component {
+
+    // 스크롤바를 맨 밑으로 이동해주는 함수.
+    scrollToBottom = () => {
+        const { scrollHeight, clientHeight } = this.box;
+        /* 위 코드는 비구조화 할당 문법을 사용한 것입니다.
+           아래 코드는 같은 의미입니다:
+           const scrollHeight = this.box.scrollHeight;
+           const clientHeight = this.box.clientHeight;
+        */
+        this.box.scrollTop = scrollHeight - clientHeight;
+    };
+    scrollToTop = () => {
+        const { scrollHeight, clientHeight } = this.box;
+        /* 위 코드는 비구조화 할당 문법을 사용한 것입니다.
+           아래 코드는 같은 의미입니다:
+           const scrollHeight = this.box.scrollHeight;
+           const clientHeight = this.box.clientHeight;
+        */
+        this.box.scrollTop = 0;
+    };
     render() {
         const style = {
             border: '1px solid black',
