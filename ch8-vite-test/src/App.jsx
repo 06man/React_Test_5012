23c102431af388f6beb5420c97aea740d5afc6df
이상용 <lsy3709@naver.com>
2025-01-17 14:05:44 +0900

    5교시, ch8 hooks 준비2

---
+++ ch8-vite-test/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='react'>ch8 hooks 함수형 컴포넌트 추가 기능 확인</h1>
+    </>
+  )
+}
+
+export default App
