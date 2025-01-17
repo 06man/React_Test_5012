67587dc5e379fd4bc5c64c7fb2b1dc4650184336
이상용 <lsy3709@naver.com>
2025-01-17 14:23:06 +0900

    5교시, ch8 hooks , useEffect ,  준비1

--- ch8-vite-test/src/App.jsx
+++ ch8-vite-test/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './component/Counter'
+import Info from './component/Info'
 
 function App() {
 
   return (
     <>
       <h1 className='react'>ch8 hooks 함수형 컴포넌트 추가 기능 확인</h1>
+      <Counter />
+      <Info />
     </>
   )
 }
---
+++ ch8-vite-test/src/component/Info.jsx
@@ -0,0 +1,43 @@
+// useEffect, 추가 1-1
+import { useState, useEffect } from 'react';
+
+const Info = () => {
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    // useEffect, 추가 1-2
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다!');
+        console.log({
+            name,
+            nickname,
+        });
+    }, [name, nickname]); // name과 nickname이 변경될 때마다 useEffect 실행
+
+    const onChangeName = (e) => {
+        setName(e.target.value);
+    };
+
+    const onChangeNickname = (e) => {
+        setNickname(e.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <input value={name} onChange={onChangeName} placeholder="이름" />
+                <input value={nickname} onChange={onChangeNickname} placeholder="닉네임" />
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b> {name}
+                </div>
+                <div>
+                    <b>닉네임:</b> {nickname}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
