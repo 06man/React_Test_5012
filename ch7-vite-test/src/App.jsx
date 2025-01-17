b18b1ecc2726af6ec6b96e8b1b2de85a641f817b
이상용 <lsy3709@naver.com>
2025-01-17 11:33:16 +0900

    3교시, ch7 생명주기 준비4

--- ch7-vite-test/src/App.jsx
+++ ch7-vite-test/src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
-import LifeCycleSample from './component/LifeCycleSample'
+import React, { Component } from 'react';
+import LifeCycleSample from './component/LifeCycleSample';
 
-function App() {
-  return (
-    <>
-      <h1 className='react'>ch7 클래스 컴포넌트에서 생명주기 메서드</h1>
-      <LifeCycleSample />
-    </>
-  )
+
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 }
 
-export default App
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
+
+export default App;
