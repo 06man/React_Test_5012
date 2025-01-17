20ec40ffce97fae10d30989d7ce1aebdf98d2ebd
이상용 <lsy3709@naver.com>
2025-01-17 11:30:27 +0900

    3교시, ch7 생명주기 준비3

--- ch7-vite-test/src/App.jsx
+++ ch7-vite-test/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import LifeCycleSample from './component/LifeCycleSample'
 
 function App() {
   return (
     <>
       <h1 className='react'>ch7 클래스 컴포넌트에서 생명주기 메서드</h1>
+      <LifeCycleSample />
     </>
   )
 }
