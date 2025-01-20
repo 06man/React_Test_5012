c525efb93b8b986654e9e38c79f4798e7c065e46
이상용 <lsy3709@naver.com>
2025-01-20 11:25:58 +0900

    3교시, ch9 Sass 문법 이용해서, 변수 및 함수 재사용 확인

--- ch9-vite-test/src/App.jsx
+++ ch9-vite-test/src/App.jsx
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-//로고 이미지 경로 변경
-import logo from './assets/react.svg';
-import './App.css';
+import SassComponent from './components/SassComponent';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SassComponent />
       </div>
     );
   }
