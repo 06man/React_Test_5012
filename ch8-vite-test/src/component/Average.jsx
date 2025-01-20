7f1d0a32f676e0c198c5284eda8cd7d04b6e5364
이상용 <lsy3709@naver.com>
2025-01-20 10:26:54 +0900

    2교시, 실습4 응용1, 마지막 추가된 요소 확인 및 css 효과 주기

--- ch8-vite-test/src/component/Average.jsx
+++ ch8-vite-test/src/component/Average.jsx
@@ -24,6 +24,10 @@ const Average = () => {
         // setNumber(e.target.value);
     };
 
+
+    // 실습4, 응용1, 마지막에 추가된 요소의 인덱스를 기억하기.
+    const [lastAddedIndex, setLastAddedIndex] = useState(null);
+
     // 실습 1-2, 공백 입력시 유효성 체크. 
     const onInsert = () => {
         // parseInt(number, 10), number 문자열로 된 숫자, 숫자 타입 변경, 
@@ -39,6 +43,9 @@ const Average = () => {
         const nextList = list.concat(parseInt(number, 10));
         setList(nextList);
         setNumber('');
+
+        // 실습 4-2 , 인덱스 추가. 최근 추가된 숫자 요소의 인덱스 
+        setLastAddedIndex(nextList.length - 1)
     };
 
     // useMemo, 추가, 기존 평균 구하는 함수를 메모 이용하기. 
@@ -46,13 +53,18 @@ const Average = () => {
     // 실습 2, 사용하기전, 콘솔 확인 해보기. 
     // 숫자 입력 중에도, 불필요한 연산을 계속함. 
     // const avg = getAverage(list);
+
+
+
     return (
         <div>
             <input value={number} onChange={onChange} placeholder="숫자를 입력하세요" />
             <button onClick={onInsert}>추가</button>
             <ul>
                 {list.map((value, index) => (
-                    <li key={index}>{value}</li>
+                    <li key={index}
+                        style={{ color: index === lastAddedIndex ? 'red' : 'black' }}
+                    >{value}</li>
                 ))}
             </ul>
             <div>
