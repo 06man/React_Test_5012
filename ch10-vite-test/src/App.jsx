5925c62493adbd754f708bfb7763e9add8a35364
이상용 <lsy3709@naver.com>
2025-01-20 17:17:12 +0900

    8교시, ch10 준비

---
+++ ch10-vite-test/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='react'>ch10 일정관리 애플리케이션 예제</h1>
+    </>
+  )
+}
+
+export default App
