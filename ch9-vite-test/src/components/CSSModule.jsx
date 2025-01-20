760dc1f5032f1b770e0e06aba6c796a4ed5d278f
이상용 <lsy3709@naver.com>
2025-01-20 12:27:12 +0900

    4교시, ch9 CSSModule 이용해서, 2개이상 클래스 적용시 예시

--- ch9-vite-test/src/components/CSSModule.jsx
+++ ch9-vite-test/src/components/CSSModule.jsx
@@ -10,7 +10,10 @@ const CSSModule = () => {
             <div className={styles.wrapper2}>
                 안녕하세요, 저는 <span className={'mycss'}>CSSModule!</span>
             </div>
-        </>
+
+            <div className={`${styles.wrapper} ${styles.inverted}`}>
+                안녕하세요, 저는 <span className={'something'}>CSSModule!</span>
+            </div>        </>
     );
 }
 
