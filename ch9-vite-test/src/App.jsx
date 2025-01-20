b4e87eb6092bb02052f61276dd0ca3e4dea98729
이상용 <lsy3709@naver.com>
2025-01-20 10:44:27 +0900

    2교시, ch9 준비2

---
+++ ch9-vite-test/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='react'>ch9 컴포넌트 스타일링</h1>
+    </>
+  )
+}
+
+export default App
