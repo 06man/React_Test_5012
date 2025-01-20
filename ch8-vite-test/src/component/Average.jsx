019670a93e8898a9387d5ea679f66338d17bc61d
이상용 <lsy3709@naver.com>
2025-01-20 10:35:24 +0900

    2교시, 실습5 응용2, 기존 배열을 1차로 filter 적용 후 새로운 배열 생성하고, 추가로 map 함수로 2차 조건 설정 연습

--- ch8-vite-test/src/component/Average.jsx
+++ ch8-vite-test/src/component/Average.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 const getAverage = (numbers) => {
     console.log('평균값 계산 중..');
@@ -29,7 +29,9 @@ const Average = () => {
     const [lastAddedIndex, setLastAddedIndex] = useState(null);
 
     // 실습 1-2, 공백 입력시 유효성 체크. 
-    const onInsert = () => {
+    // 추가로, 해당 함수를 매번 새로 생성?, 재사용?
+    // useCallback 이용하면 됨. 
+    const onInsert = useCallback(() => {
         // parseInt(number, 10), number 문자열로 된 숫자, 숫자 타입 변경, 
         // 진법 10 진법 표기. 
         // 유효성 체크 
@@ -45,8 +47,11 @@ const Average = () => {
         setNumber('');
 
         // 실습 4-2 , 인덱스 추가. 최근 추가된 숫자 요소의 인덱스 
+        // 실습 5 , 적용을 하면, 기존은 무조건 마지막 요소의 인덱스만 
+        // 추가를 했다면, 입력된 값과, 현재 평균값을 비교해서, 다시 
+        // 인덱스를 재설정 해야함. 
         setLastAddedIndex(nextList.length - 1)
-    };
+    }, [number, list]);
 
     // useMemo, 추가, 기존 평균 구하는 함수를 메모 이용하기. 
     const avg = useMemo(() => getAverage(list), [list]);
@@ -61,11 +66,17 @@ const Average = () => {
             <input value={number} onChange={onChange} placeholder="숫자를 입력하세요" />
             <button onClick={onInsert}>추가</button>
             <ul>
-                {list.map((value, index) => (
-                    <li key={index}
-                        style={{ color: index === lastAddedIndex ? 'red' : 'black' }}
-                    >{value}</li>
-                ))}
+                {/* 실습 5, 응용2, 평균값 기준 필터링, filter 내장함수 이용 */}
+                {list
+                    // 1차로, list에서 각 요소를 다 순회 하면서, 평균 보다 큰 요소를 
+                    // 따로 뽑아서 새로운 배열 생성
+                    .filter((value) => value > avg)
+                    // 1차로 , 새롭게 뽑은 배열을 가지고, 다시 map 함수를 사용.
+                    .map((value, index) => (
+                        <li key={index}
+                            style={{ color: index === lastAddedIndex ? 'red' : 'black' }}
+                        >{value}</li>
+                    ))}
             </ul>
             <div>
                 {/* <b>평균값:</b> {getAverage(list)} */}
