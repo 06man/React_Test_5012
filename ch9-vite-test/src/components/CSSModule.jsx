ff7bead3a8bb6009370a40a6f73fe7931dcf5d98
이상용 <lsy3709@naver.com>
2025-01-20 14:03:22 +0900

    5교시, ch9 CSSModule 이용해서, bind 함수 이용해서, 더 쉽게 클래스 이름에 접근하기

--- ch9-vite-test/src/components/CSSModule.jsx
+++ ch9-vite-test/src/components/CSSModule.jsx
@@ -1,19 +1,15 @@
+import classNames from 'classnames/bind';
+// import styles from './CSSModule.module.css';
 import styles from '@styles/CSSModule.module.css';
-// import '@styles/SassComponent.scss';
+
+const cx = classNames.bind(styles);
+// 미리 styles에서 클래스를 받아 오도록 설정하고
 
 const CSSModule = () => {
     return (
-        <>
-            <div className={styles.wrapper}>
-                안녕하세요, 저는 <span className={'something'}>CSSModule!</span>
-            </div>
-            <div className={styles.wrapper2}>
-                안녕하세요, 저는 <span className={'mycss'}>CSSModule!</span>
-            </div>
-
-            <div className={`${styles.wrapper} ${styles.inverted}`}>
-                안녕하세요, 저는 <span className={'something'}>CSSModule!</span>
-            </div>        </>
+        <div className={cx('wrapper', 'inverted')}>
+            안녕하세요, 저는 <span className={styles.something}>CSSModule!</span>
+        </div>
     );
 }
 
