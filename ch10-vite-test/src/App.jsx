9d175e498ad8f0d55ab21d7481507310f7d36916
이상용 <lsy3709@naver.com>
2025-01-21 09:19:18 +0900

    1/21 1교시, 일정관리 , 화면 작업1 TodoTemplate

--- ch10-vite-test/src/App.jsx
+++ ch10-vite-test/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
+import TodoTemplate from './components/TodoTemplate'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <h1 className='react'>ch10 일정관리 애플리케이션 예제</h1>
-      <div>Todo App을 만들자!</div>
+      <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
     </>
   )
 }
