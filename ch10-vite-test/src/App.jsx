5397c96cb17c782640a9b2b8ad344b860b745436
이상용 <lsy3709@naver.com>
2025-01-21 09:49:39 +0900

    1교시, 일정관리 , 기능 작업1 App에서 더미 데이터 todos, TodoList 에전달, 아직 아이템은 변경 안함

--- ch10-vite-test/src/App.jsx
+++ ch10-vite-test/src/App.jsx
@@ -6,7 +6,24 @@ import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
 
 function App() {
-
+  // 더미 데이터 추가1
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
 
   return (
     <>
@@ -14,7 +31,8 @@ function App() {
       <TodoTemplate>
         {/* Todo App을 만들자! */}
         <TodoInsert />
-        <TodoList />
+        {/* 더미 데이터 추가2  props 로 전달. */}
+        <TodoList todos={todos} />
       </TodoTemplate>;
 
     </>
