480fa336c63c21606a3d6ebd5663d99d01046188
이상용 <lsy3709@naver.com>
2025-01-21 10:42:08 +0900

    2교시, 일정관리 , 기능 작업1, 자식에서 onInsert 함수를 받아서 적용하기, 기능확인

--- ch10-vite-test/src/components/TodoInsert.jsx
+++ ch10-vite-test/src/components/TodoInsert.jsx
@@ -5,8 +5,9 @@ import { MdAdd } from 'react-icons/md';
 // import { IoMdAirplane } from "react-icons/io";
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
-    //추가2
+// 추가2-1 , onInsert(함수)
+const TodoInsert = ({ onInsert }) => {
+    //추가1-2
     const [value, setValue] = useState('');
 
     // useCallback(콜백함수, []), []:함수를 한번 생성, 
@@ -14,15 +15,29 @@ const TodoInsert = () => {
     const onChange = useCallback((e) => {
         setValue(e.target.value);
     }, []);
-    //추가2
+    //추가1-2
+
+    // 추가2-2, useCallback-> 함수를 새롭게 생성함.
+    const onSubmit = useCallback(
+        (e) => {
+            onInsert(value);
+            setValue(''); // value 값 초기화
+
+            // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+            // 이를 방지하기 위해 이 함수를 호출합니다.
+            e.preventDefault();
+        },
+        [onInsert, value]
+    );
 
     return (
-        <form className="TodoInsert">
+        // 추가2-3, onSubmit={onSubmit
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input placeholder="할 일을 입력하세요"
-                // 추가3
+                // 추가1-3
                 value={value}
                 onChange={onChange} />
-            {/* 추가3 */}
+            {/* 추가1-3 */}
             <button type="submit">
                 <MdAdd />
                 {/* <IoMdAirplane /> */}
