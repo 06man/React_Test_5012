a773ce5e7f52f2d8519ce4873a88557202e6db7c
이상용 <lsy3709@naver.com>
2025-01-21 10:15:30 +0900

    2교시, 일정관리 , 기능 작업1, TodoListItem 요소 부분 체크시 취소선 및 아이콘 색 확인

--- ch10-vite-test/src/components/TodoListItem.jsx
+++ ch10-vite-test/src/components/TodoListItem.jsx
@@ -1,12 +1,26 @@
-import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md';
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+// 추가1
+// 클래스 이름 쉽게 접근 및 조건부 렌더링 하는 도구 
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+// 추가2, { todo } ->  
+// { id: 1, text: '리액트의 기초 알아보기',checked: true,},
+const TodoListItem = ({ todo }) => {
+    const { text, checked } = todo;
+
     return (
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-                <div className="text">할 일</div>
+            {/* 추가3 */}
+            <div className={cn('checkbox', { checked })}>
+                {/* 추가4 */}
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                {/* 추가5 */}
+                <div className="text">{text}</div>
             </div>
             <div className="remove">
                 <MdRemoveCircleOutline />
