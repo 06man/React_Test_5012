b80bfa30409f959450060c681648d854d994e55f
이상용 <lsy3709@naver.com>
2025-01-22 14:24:06 +0900

    5교시, 리액트 라우팅 기본 확인, home, about 페이지 이동 기본1

--- ch13-vite-test/src/App.jsx
+++ ch13-vite-test/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+// 준비물1
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
       <h1 className='react'>ch13 리액트 라우팅</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   )
 }
---
+++ ch13-vite-test/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <p>가장 먼저 보여지는 페이지입니다.</p>
+        </div>
+    );
+};
+
+export default Home;
