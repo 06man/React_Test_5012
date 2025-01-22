de94e71d788353526e4c53483cf5f59b8fff5876
이상용 <lsy3709@naver.com>
2025-01-22 14:29:11 +0900

    5교시, 리액트 라우팅 기본 확인, 링크 이동 Link 예시1

--- ch13-vite-test/src/pages/Home.jsx
+++ ch13-vite-test/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
     return (
         <div>
             <p>가장 먼저 보여지는 페이지입니다.</p>
+            {/* 추가 */}
+            <Link to="/about">소개</Link>
         </div>
     );
 };
