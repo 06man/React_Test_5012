a9773fe608dbd66d45c9d0c27dc829e96031d1d8
이상용 <lsy3709@naver.com>
2025-01-23 09:30:52 +0900

    1교시, ch14 API 연동, 준비2

--- ch14-vite-test/src/App.jsx
+++ ch14-vite-test/src/App.jsx
@@ -1,14 +1,33 @@
-
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
 
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        setData(response.data);
+      });
+  };
 
   return (
-    <>
+    <div>
       <h1 className='react'>ch14 API 연동해서 뷰어 만들기</h1>
-    </>
-  )
-}
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
