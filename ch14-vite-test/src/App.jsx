ce180b0b4461e40cc5ffa05bdbeb46bbb3e3781d
이상용 <lsy3709@naver.com>
2025-01-23 10:06:23 +0900

    2교시, ch14 API 연동, 준비3, 뉴스 API 구조 파악, 뷰어 화면 작업1

--- ch14-vite-test/src/App.jsx
+++ ch14-vite-test/src/App.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css'
+import NewsList from './components/NewsList';
 
 const App = () => {
   const [data, setData] = useState(null);
 
   const onClick = () => {
     axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .get('https://newsapi.org/v2/top-headlines?country=us&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f')
       .then((response) => {
         setData(response.data);
       });
+    // const response = await axios.get(
+    //   'https://newsapi.org/v2/top-headlines?country=us&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f'
+    // );
   };
 
   return (
     <div>
       <h1 className='react'>ch14 API 연동해서 뷰어 만들기</h1>
-      <div>
+      {/* <div>
         <button onClick={onClick}>불러오기</button>
       </div>
       {data && (
@@ -25,7 +29,8 @@ const App = () => {
           value={JSON.stringify(data, null, 2)}
           readOnly={true}
         />
-      )}
+      )} */}
+      <NewsList />
     </div>
   );
 };
