4f2b3ad5da2cd06c4e98014c545348f7164da7a5
이상용 <lsy3709@naver.com>
2025-01-23 12:39:34 +0900

    4교시, ch14 API 연동, 커스텀 훅스 이용해서, 로직 파일 분리하기.

--- ch14-vite-test/src/components/NewsList.jsx
+++ ch14-vite-test/src/components/NewsList.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,37 +18,49 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
+    // 방법1
+    // 한 파일 내에 , 로직이 포함 된 경우
+
+    // //추가
+    // const [articles, setArticles] = useState(null);
+    // const [loading, setLoading] = useState(false);
+    // //추가
+    // // 마운트시, 최초에 한번만 데이터를 받아오자, 함수형 컴포넌트 
+    // // 생명주기. 
+    // useEffect(() => {
+    //     // async를 사용하는 함수 선언
+    //     const fetchData = async () => {
+    //         setLoading(true);
+    //         try {
+    //             // 방법1, 전체기사에 대한 데이터 받기. 
+    //             // const response = await axios.get(
+    //             //     'https://newsapi.org/v2/top-headlines?country=us&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f'
+    //             // );
+    //             // 방법2, 각 카테고리별로 받기.
+    //             //추가
+    //             const query = category === 'all' ? '' : `&category=${category}`;
+    //             //추가       
+    //             const response = await axios.get(
+    //                 `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`
+    //             );
+    //             setArticles(response.data.articles);
+    //         } catch (e) {
+    //             console.log(e);
+    //         }
+    //         setLoading(false);
+    //     };
+    //     // 함수를 이용해야. 
+    //     fetchData();
+    //     //category 변경시 마다 , useEffect 함수가 동작함.
+    // }, [category]);
+
+    // 방법2, 파일 분리해서, 작업. 커스텀 훅스 이용. 
     //추가
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-    //추가
-    // 마운트시, 최초에 한번만 데이터를 받아오자, 함수형 컴포넌트 
-    // 생명주기. 
-    useEffect(() => {
-        // async를 사용하는 함수 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                // 방법1, 전체기사에 대한 데이터 받기. 
-                // const response = await axios.get(
-                //     'https://newsapi.org/v2/top-headlines?country=us&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f'
-                // );
-                // 방법2, 각 카테고리별로 받기.
-                //추가
-                const query = category === 'all' ? '' : `&category=${category}`;
-                //추가       
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`
-                );
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        // 함수를 이용해야. 
-        fetchData();
-        //category 변경시 마다 , useEffect 함수가 동작함.
+    const [loading, resolved, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`
+        );
     }, [category]);
 
     //추가
@@ -58,10 +71,22 @@ const NewsList = ({ category }) => {
 
     //추가
     // 아직 articles 값이 설정되지 않았을 때
-    if (!articles) {
+
+    // 교체1, 커스텀 훅스. 
+    // if (!articles) {
+    if (!resolved) {
         // 데이터가 받아 온게 없으면 , 화면에 그리지 않는다.
         return null;
     }
+    //추가, 커스텀 훅스
+    // 에러가 발생했을 때
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>;
+    }
+
+    //추가, 커스텀 훅스
+    // resolved 값이 유효할 때
+    const { articles } = resolved.data;
 
     // articles 값이 유효할 때
     return (
