9075efe7d2ea255a8738d71d6eba3bd5e0ecee31
이상용 <lsy3709@naver.com>
2025-01-23 12:09:32 +0900

    4교시, ch13 라우팅 실습 6 풀이

--- ch13-vite-test/src/pages/Category.jsx
+++ ch13-vite-test/src/pages/Category.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, useSearchParams, Outlet } from 'react-router-dom';
+import { useParams, useSearchParams, Outlet, useNavigate } from 'react-router-dom';
 
 // 실습1
 const Category = () => {
@@ -11,16 +11,22 @@ const Category = () => {
     //http://localhost:5173/category/lsy?username=이상용
     const [searchParams] = useSearchParams();
     const username = searchParams.get('username')
-
+    //실습 6,
+    const navigate = useNavigate();
+    const goHome = () => {
+        navigate('/')
+    }
     return (
+
         <div>
+            <button onClick={goHome}>홈가기</button>
             <h1>Category 페이지입니다.</h1>
             <h2>파라미터 받기 예시 useParams() 이용: {name} </h2>
             <h2>쿼리 스트링 받기 예시 useSearchParams() 이용: {username} </h2>
             <h2>중첩 라우팅 화면 표시 </h2>
             {/* 실습3  */}
             <Outlet />
-        </div>
+        </div >
     );
 };
 
