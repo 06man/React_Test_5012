d408deb2178ae5b097d69f536992b7fb77eef2a4
이상용 <lsy3709@naver.com>
2025-01-24 14:40:58 +0900

    5교시, context API 준비2, 전역 이용하기, 객체, 함수 전달

--- ch15-vite-test/src/App.jsx
+++ ch15-vite-test/src/App.jsx
@@ -1,5 +1,8 @@
 
 import './App.css'
+import ColorBox from './components/ColorBox'
+import SelectColors from './components/SelectColors'
+import ColorContext, { ColorProvider } from './contexts/color'
 
 function App() {
 
@@ -7,6 +10,16 @@ function App() {
   return (
     <>
       <h1 className='react'>ch15 context API 전역 이용하기</h1>
+      {/* <ColorContext.Provider value={{ color: 'blue' }}>
+        <ColorBox />
+      </ColorContext.Provider> */}
+      {/* 방법2, 적용하기, */}
+      <ColorProvider>
+        <div>
+          <SelectColors />
+          <ColorBox />
+        </div>
+      </ColorProvider>
     </>
   )
 }
